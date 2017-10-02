e465100273f3c49f1a5ed47965aafd81d4c48a48
screenlantern <screenlantern@gmail.com>
2017-10-02 21:44:04 +0100

    added space between fname & lname

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
     return(
         <div className="Card"> 
             <div className="Card__avatar"><img src={props.info.picture.large} alt="avatar" /></div>
-            <p className="Card__name">{props.info.name.first}  {props.info.name.last}</p>
+            <p className="Card__name">{props.info.name.first}&nbsp;{props.info.name.last}</p>
             <p className="Card__email">{props.info.email}</p>
             <h5>Joined</h5>
             <p>{props.reg}</p>
